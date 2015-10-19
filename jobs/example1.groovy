7433277e9a956287f1db7a6aa5716a5331d3bf86
Thomas Beyens <thomas.beyens@tass.be>
2015-10-19 12:37:55 +0200

    Adding first example for seed job.

---
+++ jobs/example1.groovy
@@ -0,0 +1,21 @@
+String basePath = 'example1'
+String repo = 'tass-belgium/picotcp'
+
+folder(basePath) {
+    description 'This example shows basic folder/job creation.'
+}
+
+job("$basePath/pico-example-build") {
+    description{
+
+    }
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        shell 'echo Test'
+    }
+}
