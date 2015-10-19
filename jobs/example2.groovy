b2f5864a9b6f897b4a96d1856858b6503e9753dc
Thomas Beyens <thomas.beyens@tass.be>
2015-10-19 15:01:58 +0200

    Added imports

--- jobs/example2.groovy
+++ jobs/example2.groovy
@@ -1,7 +1,13 @@
+import groovy.json.JsonSlurper
 String repo = 'tass-belgium/picotcp'
 String slave = 'normal'
 String job1_descr = 'Perform a PicoTCP build'
 String job2_descr = 'Run unit tests on PicoTCP'
+String basePath = 'example2'
+
+folder(basePath) {
+    description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
+}
 
 URL branchUrl = "https://api.github.com/repos/$repo/branches".toURL()
 List branches = new JsonSlurper().parse(branchUrl.newReader())
