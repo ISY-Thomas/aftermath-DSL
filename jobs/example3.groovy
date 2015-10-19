48f426a71f44e90d8f796982656b0a1a3fee7022
Thomas Beyens <thomas.beyens@tass.be>
2015-10-19 19:11:40 +0200

    Removed branch.name.

--- jobs/example3.groovy
+++ jobs/example3.groovy
@@ -1,6 +1,7 @@
 String basePath = 'example3'
 String repo = 'tass-belgium/picotcp'
 String slave = 'normal'
+String branch = 'development'
 String job1_descr = 'Perform a PicoTCP build'
 String job2_descr = 'Run unit tests and make on PicoTCP'
 String job3_descr = 'Run autotests and make on PicoTCP'
@@ -9,63 +10,63 @@ folder(basePath) {
     description 'This example shows how to create a job in its own folder. With a buildflow job to control it. And the exclusion plugin.'
 }
 
-    job("$basePath/pico-build") {
-        description(job1_descr)
-        label(slave)
-        scm {
-            github repo, branch.name
-        }
-        steps {
-            shell 'make clean'
-            shell 'make'
-        }
+job("$basePath/pico-build") {
+    description(job1_descr)
+    label(slave)
+    scm {
+        github repo, branch
     }
+    steps {
+        shell 'make clean'
+        shell 'make'
+    }
+}
 
-    job("$basePath/pico-units") {
-        description(job2_descr)
-        label(slave)
-        scm {
-            github repo, branch.name
-        }
-        wrappers{
-            exclusionResources('test')
-        }
-        steps {
-            shell 'make clean'
-            shell 'make units ARCH=faulty'
-            criticalBlock{
-                shell 'test/units.sh'
-            }
+job("$basePath/pico-units") {
+    description(job2_descr)
+    label(slave)
+    scm {
+        github repo, branch
+    }
+    wrappers{
+        exclusionResources('test')
+    }
+    steps {
+        shell 'make clean'
+        shell 'make units ARCH=faulty'
+        criticalBlock{
+            shell 'test/units.sh'
         }
     }
+}
 
-    job("$basePath/pico-autotest") {
-        description(job3_descr)
-        label(slave)
-        scm {
-            github repo, branch.name
-        }
-        wrappers{
-            exclusionResources('test')
-        }
-        steps {
-            shell 'make clean'
-            shell 'make make test'
-            criticalBlock{
-                shell './test/autotest.sh'
-            }
+job("$basePath/pico-autotest") {
+    description(job3_descr)
+    label(slave)
+    scm {
+        github repo, branch
+    }
+    wrappers{
+        exclusionResources('test')
+    }
+    steps {
+        shell 'make clean'
+        shell 'make make test'
+        criticalBlock{
+            shell './test/autotest.sh'
         }
     }
+}
 
-    buildFlowJob("$basePath/pico-buildflow") {
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    buildFlow("""
-    build("pico-build")
-    parallel(
-      build("pico-units")
-      build("pico-autotest")
-    )
-    """)
-    }
+buildFlowJob("$basePath/pico-buildflow") {
+triggers {
+    scm 'H/5 * * * *'
+}
+buildFlow("""
+build("pico-build")
+parallel(
+  build("pico-units")
+  build("pico-autotest")
+)
+""")
+}
